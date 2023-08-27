338c502fa766a8429b13965b3219f6e4f1406d48
hollow-d9d <taronhar@gmail.com>
2023-08-27 19:55:43 +0400

    Activate button script, localization settings, localization tables, custom font

---
+++ Assets/Resources/Scripts/activate_button.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class activate_button : MonoBehaviour
+{
+    [SerializeField] private Button button;
+    [SerializeField] private TextMeshProUGUI text1;
+    [SerializeField] private TextMeshProUGUI text2;
+    private void OnMouseDown()
+    {
+        button.interactable = true;
+        text1.gameObject.SetActive(false);
+        text2.gameObject.SetActive(true);
+
+    }
+
+}
